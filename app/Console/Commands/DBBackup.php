0d4b05b9dc7a15b1f7497453501a7e33021310d9
ilz2010 <ilz2010@yandex.ru>
2016-12-19 20:39:04 +0500

    Actor and Article controller FIX

---
+++ app/Console/Commands/DBBackup.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DBBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        printf("Backup done \n");
+    }
+}
