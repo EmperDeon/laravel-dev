5f0b2e42d2c11b99498fc224b8c531a6073336d2
ilz2010 <ilz2010@yandex.ru>
2016-12-24 17:30:01 +0500

    Big changes FIX 2

--- database/seeds/PosterSeeder.php
+++ database/seeds/PosterSeeder.php
@@ -3,9 +3,15 @@
 use App\Poster;
 use Carbon\Carbon;
 use App\Interfaces\TS;
+use Jenssegers\Date\Date;
 
 class PosterSeeder extends TS
 {
+    public function date($d)
+    {
+        return Date::parse($d)->format('d.m.Y H:i');
+    }
+
     /**
      * Run the database seeds.
      *
@@ -15,26 +21,26 @@ class PosterSeeder extends TS
     {
         $d = Carbon::today()->addHours(19);
 
-        Poster::create(['hall_id' => TS::id(1), 't_perf_id' => TS::id(1), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(2), 't_perf_id' => TS::id(2), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(5), 't_perf_id' => TS::id(5), 'date' => $d]);
+        Poster::create(['hall_id' => TS::id(1), 't_perf_id' => TS::id(1), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(2), 't_perf_id' => TS::id(2), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(5), 't_perf_id' => TS::id(5), 'date' => $this->date($d)]);
 
         $d->addDays(5);
-        Poster::create(['hall_id' => TS::id(2), 't_perf_id' => TS::id(2), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(1), 't_perf_id' => TS::id(3), 'date' => $d->addHours(1)]);
-        Poster::create(['hall_id' => TS::id(3), 't_perf_id' => TS::id(2), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(4), 't_perf_id' => TS::id(5), 'date' => $d->addHours(1)]);
+        Poster::create(['hall_id' => TS::id(2), 't_perf_id' => TS::id(2), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(1), 't_perf_id' => TS::id(3), 'date' => $this->date($d->addHours(1))]);
+        Poster::create(['hall_id' => TS::id(3), 't_perf_id' => TS::id(2), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(4), 't_perf_id' => TS::id(5), 'date' => $this->date($d->addHours(1))]);
 
         $d->subHours(2)->addMonth(1);
-        Poster::create(['hall_id' => TS::id(3), 't_perf_id' => TS::id(1), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(4), 't_perf_id' => TS::id(4), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(5), 't_perf_id' => TS::id(5), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(6), 't_perf_id' => TS::id(6), 'date' => $d]);
+        Poster::create(['hall_id' => TS::id(3), 't_perf_id' => TS::id(1), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(4), 't_perf_id' => TS::id(4), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(5), 't_perf_id' => TS::id(5), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(6), 't_perf_id' => TS::id(6), 'date' => $this->date($d)]);
 
         $d->addMonth(1);
-        Poster::create(['hall_id' => TS::id(3), 't_perf_id' => TS::id(1), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(4), 't_perf_id' => TS::id(4), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(5), 't_perf_id' => TS::id(5), 'date' => $d]);
-        Poster::create(['hall_id' => TS::id(6), 't_perf_id' => TS::id(6), 'date' => $d]);
+        Poster::create(['hall_id' => TS::id(3), 't_perf_id' => TS::id(1), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(4), 't_perf_id' => TS::id(4), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(5), 't_perf_id' => TS::id(5), 'date' => $this->date($d)]);
+        Poster::create(['hall_id' => TS::id(6), 't_perf_id' => TS::id(6), 'date' => $this->date($d)]);
     }
 }
